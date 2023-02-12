70aaea1d391a4eff47883550de273734ea35790a
Buziu <mateusz.buzalski@gmail.com>
2023-02-12 14:33:51 +0100

    tagClickHandler first steps

--- js/script.js
+++ js/script.js
@@ -114,11 +114,13 @@
     /* prevent default action for this event */
     event.preventDefault();
     /* make new constant named "clickedElement" and give it the value of "this" */
-  
+    const clickedElement = this;
+    console.log('Link was clicked!');
     /* make a new constant "href" and read the attribute "href" of the clicked element */
-  
+    const href = clickedElement.getAttribute('href');
+    console.log(href);
     /* make a new constant "tag" and extract tag from the "href" constant */
-  
+    const tag = href.replace('#tag-','');
     /* find all tag links with class active */
   
     /* START LOOP: for each active tag link */
